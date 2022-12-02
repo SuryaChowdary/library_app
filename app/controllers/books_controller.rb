06c7dc8c37fe45948455c85d1c8b4ec214ef4789
surya <surya@singular.dev>
2022-12-02 15:03:18 +0530

    creating new Books from browser is added

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,26 @@
 class BooksController < ApplicationController
+
+
   def show 
     @book = Library.find(params[:id])
   end
+
+
   def index 
     @books = Library.all
   end 
-  def edit 
+  
+  def new 
+    @book = Library.new
+  end 
+
+
+  def create 
+    @book=Library.new(params.require(:book).permit(:title ,:author ,:pages , :date_of_publication, :summary , :rent))
+    if @book.save
+      flash[:notice] = "Book is added successfully"
+    render plain: @book.inspect
+    else render 'new'
+    end
   end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about' , to: 'pages#about'
-  resources :books ,only: [:show , :index ]
+  resources :books ,only: [:show , :index ,:new, :create]
 end
